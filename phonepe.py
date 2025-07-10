d0b905c227d1f8545af856a14fbc494b5f135728
sathi000000007 <sathi7019756776@gmail.com>
2025-07-10 11:37:04 +0530

    Create phonepe.py

---
+++ phonepe.py
@@ -0,0 +1,32 @@
+print("1.whats exciting about the phonepay HDFC co-branded card")
+print("2.how do i add a bank account on phonepe")
+print("3.how do i add a money to my phonepe wallet")
+print("4.how do i apply for a loan on phonepe")
+print("5.when will my units be allocated after i invest in mutual funds")
+print("6.how do i find my insurance policy document.")
+user_input=int(input("enter your choice"))
+if user_input==1:
+  print("its our latest launch")
+elif user_input==2:
+  print("tap on profile picture")
+  print("1.what if am unable to add my bank account on phoepe")
+  print("2.what if the sms verification fais")
+  print("3.how do i update the mobile with my bank account")
+  user_input=int(input("enter your choice"))
+  if user_input==1:
+    print("1.message")
+  if user_input==2:
+    print("2.message")
+  elif user_input==3:
+    print("3.messsage")
+elif user_input==3:
+  print("realated topics")
+  print("how do i check my balance on phonpe wallet")
+  print("how old i should be to use phonepe")
+  user_input=int(input("enter your choice"))
+  if user_input==1:
+    print("message 1")
+  if user_input==2:
+    print("2.message")
+  elif user_input==3:
+    print("3.messsage")
